4071406e972daad24e74338e51effd68c60e5104
Gabriel Borralho <39606678+gabrielrhcp@users.noreply.github.com>
2019-10-17 08:38:03 -0300

    Update quadrado.lua
    correção de erros e otimização do código

--- quadrado.lua
+++ quadrado.lua
@@ -1,15 +1,4 @@
 print("Informe um numero:")
 num = io.read("*n")
 
-cont=1
-prox=1
-quad=1
-
-while cont < num and num >1 do 
-  cont=cont+1
-  prox=prox+2
-  quad=quad+prox
-end
-
-print(quad)
-jhbhbjhb
+print("O quadrado deste numero é: ".. num*num)
